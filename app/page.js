77adeb5ed1e4d7166ed7f520b80357edda5daac1
unknown <Mariventhri@gmail.com>
2024-02-16 15:47:19 +0800

    remove ad and up vs email

--- app/page.js
+++ app/page.js
@@ -3,13 +3,12 @@ import Coverpage from '@/components/Coverpage'
 import Rooms from '@/components/Rooms';
 import Sports from '@/components/Sports';
 import Experience from '@/components/Experience';
-import AdsModal from '@/components/Modal';
+// import AdsModal from '@/components/Modal';
 import IgWidget from '@/components/Widget';
 
 export default function Home() {
   return (
       <>
-        <AdsModal />
         <Coverpage />
         <Rooms />
         <Sports />
